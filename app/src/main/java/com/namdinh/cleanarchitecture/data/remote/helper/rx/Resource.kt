64ee0c158adec01359da912b04507ccfc5ab6a96
Nam Dinh <nam.dinh@mingle.com>
2018-10-26 16:41:56 +0700

    [f] add loading_state.xml

--- app/src/main/java/com/namdinh/cleanarchitecture/data/remote/helper/rx/Resource.kt
+++ app/src/main/java/com/namdinh/cleanarchitecture/data/remote/helper/rx/Resource.kt
@@ -1,9 +1,18 @@
 package com.namdinh.cleanarchitecture.data.remote.helper.rx
 
+import androidx.databinding.Bindable
+
+
 sealed class Resource<out T> {
     class Loading<out T> : Resource<T>()
 
     data class Success<out T>(val data: T?) : Resource<T>()
 
     data class Failure<out T>(val throwable: Throwable) : Resource<T>()
+
+    @Bindable
+    fun getFailureMessage(): String? {
+        (this as? Failure)?.throwable?.message
+        return null
+    }
 }
